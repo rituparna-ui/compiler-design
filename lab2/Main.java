2ec26fad0d21906d4ef3789911258c25294e398c
Rituparna W <rituparnaw16@gmail.com>
2023-01-25 09:22:58 +0530

    Lab 2

---
+++ lab2/Main.java
@@ -0,0 +1,13 @@
+// this is c public Main class
+
+public class Main {
+  // this is main method
+
+  /**
+   * 
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("xD");
+  }
+}
